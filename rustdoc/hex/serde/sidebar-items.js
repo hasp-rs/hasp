c2f6ecc1eb002943322217d00ce9f8ab6c4da030
sunshowers <rain@sunshowers.io>
2021-10-27 22:09:55 +0000

    Deploying to gh-pages from  @ 6e8f5f6a7bd01f68d7f73ef957d1e2245159ec9e 🚀

---
+++ rustdoc/hex/serde/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["deserialize","Deserializes a hex string into raw bytes."],["serialize","Serializes `data` as hex string using lowercase characters."],["serialize_upper","Serializes `data` as hex string using uppercase characters."]]});
